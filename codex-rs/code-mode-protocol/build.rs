09bf7b9cc4f7de993c1fc081beedf98cecf8c3f9
cloudishBenne <cloudishBenne@users.noreply.github.com>
2026-09-03 04:00:41 +0200

    build(termux): optimize cargo release profile and protoc resolution

--- codex-rs/code-mode-protocol/build.rs
+++ codex-rs/code-mode-protocol/build.rs
@@ -5,7 +5,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rerun-if-changed=src/grpc");
 
     let mut config = tonic_prost_build::Config::new();
-    config.protoc_executable(protoc_bin_vendored::protoc_bin_path()?);
+    config.protoc_executable(std::path::PathBuf::from("/data/data/com.termux/files/usr/bin/protoc"));
     let proto_files = glob::glob("src/grpc/*.proto")?.collect::<Result<Vec<_>, _>>()?;
 
     tonic_prost_build::configure()
